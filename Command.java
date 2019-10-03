68afc7f9f6ae99ad63ab5cc94a8191a1a89deb0c
kaushalAryan <56018775+kaushalAryan@users.noreply.github.com>
2019-10-03 22:24:08 +0530

    Update Command.java

--- Command.java
+++ Command.java
@@ -1,14 +1,10 @@
-
-
-
-
 public class Command {
 
 	public static void main(String[] args) 
 	{
 		 Scanner sc=new Scanner(System.in);
-		    //System.out.println("enter the number");
-		 //  String s=sc.nextLine();
+		   
+		  
 		  System.out.println(args[0]);
 		
 
