e78d20e9c5b311dc3068a3769c31e029f98403bb
kaushalAryan <56018775+kaushalAryan@users.noreply.github.com>
2019-10-03 21:45:04 +0530

    Add files via upload
    command line program

---
+++ Command.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class Command {
+
+	public static void main(String[] args) 
+	{
+		 Scanner sc=new Scanner(System.in);
+		    //System.out.println("enter the number");
+		 //  String s=sc.nextLine();
+		  System.out.println(args[0]);
+		
+
+	}
+
+}
