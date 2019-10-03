3ba3400945880b3f51cc3666d32d1f432cf14b1b
kaushalAryan <56018775+kaushalAryan@users.noreply.github.com>
2019-10-03 13:02:07 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,25 +1,21 @@
 
+public class Palindrome 
+{
 
-
-
-public class Palindrome {
-
-	public static void main(int[] args)
+	public static void main(String args[])
 	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the value");
-		int n = sc.nextInt();
+	int n=Integer.parseInt(args[0]);
 		
 		int sum=0;
 		int rem;
 		int  temp;
 		temp=n;
 		
-		while(args[0]>0)
+		while(n>0)
 		{
-			rem=args[0]%10;
+			rem=n%10;
 			sum=sum*10+rem;
-			args[0]=args[0]/10;
+			n=n/10;
 			
 		}
 		if(temp==sum)
