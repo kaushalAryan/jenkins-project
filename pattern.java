c399ba65a744fcd7550e61397c04fbcb37239229
kaushalAryan <56018775+kaushalAryan@users.noreply.github.com>
2019-10-03 11:41:53 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,8 +1,9 @@
-package test;
 
-import java.util.Scanner;
 
-public class pattern {
+//import java.util.Scanner;
+
+public class pattern
+{
 
 	public static void main(String[] args) {
 		//Scanner sc=new Scanner(System.in);
