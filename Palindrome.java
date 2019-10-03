134e005e3f8dce6ba15506f0b8580bbc3a59e8f7
kaushalAryan <56018775+kaushalAryan@users.noreply.github.com>
2019-10-03 12:24:41 +0530

    Add files via upload
    palindrome

---
+++ Palindrome.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value");
+		int n = sc.nextInt();
+		
+		int sum=0;
+		int rem;
+		int  temp;
+		temp=n;
+		
+		while(n>0)
+		{
+			rem=n%10;
+			sum=sum*10+rem;
+			n=n/10;
+			
+		}
+		if(temp==sum)
+		{
+			System.out.println("number is palindrome");		
+		}
+		else
+		{
+			System.out.println("number is not palindrome");	
+		}
+		
+
+	}
+
+}
